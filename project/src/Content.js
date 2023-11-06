edc2d156c3b00b3ffd4726ee65a06d9a72240274
Mukeshkhanna11 <mukeshmass.9865@gmail.com>
2023-11-06 15:40:37 +0530

    change content.js

--- project/src/Content.js
+++ project/src/Content.js
@@ -1,15 +1,28 @@
 import React from "react";
+import { useState } from "react";
 
 const Content = () => {
+  const [name, setName] = useState("dave");
+  const [count, setCount] = useState(0);
   function nameoOfChange() {
-    const name = ["rajesh", "ramesh", "suresh"];
+    const names = ["rajesh", "ramesh", "suresh"];
     let n = Math.floor(Math.random() * 3);
-    return name[n];
+    setName(names[n]);
   }
+  const handleClick = () => {
+    setCount(count + 1);
 
+    console.log(count);
+  };
+  const handleClick2 = () => {
+    console.log(count);
+  };
   return (
     <main>
-      <p>hi there {nameoOfChange()}</p>
+      <p>hi there {name}!</p>
+      <button onClick={nameoOfChange}>change name</button>
+      <button onClick={handleClick}> click here</button>
+      <button onClick={handleClick2}>click button</button>
     </main>
   );
 };
